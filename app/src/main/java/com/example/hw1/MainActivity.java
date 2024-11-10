e26d178a7ad7927676dc2802e6812fad0fee812f
Zarema09 <omoraalieva.09@icloud.com>
2024-11-10 18:01:13 +0600

    Initial commit

---
+++ app/src/main/java/com/example/hw1/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.hw1;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView tvCounter;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvCounter = findViewById(R.id.tvCounter);
+        Button btnIncrease = findViewById(R.id.btnIncrease);
+        Button btnSent = findViewById(R.id.btnSent);
+
+        btnIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                tvCounter.setText(String.valueOf(count));
+            }
+        });
+
+        btnSent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendWhatsAppMessage();
+            }
+        });
+    }
+
+    private void sendWhatsAppMessage() {
+        String friendPhoneNumber = "0777345464";
+        String message = "Количество нажатий" + count;
+        try {
+            Uri uri = Uri.parse("https://wa.me-" + friendPhoneNumber + "?text =" + Uri.encode(message));
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
